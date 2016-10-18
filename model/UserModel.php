75b5262ba2624e7c6cba50e71922e0275aa8d86d
Oscar Nordquist <on222bx@student.lnu.se>
2016-10-18 21:18:37 +0200

    Change check order

--- model/UserModel.php
+++ model/UserModel.php
@@ -13,23 +13,27 @@ class UserModel {
 
     private function validateUsername() {
         preg_match('/^[a-zA-Z0-9]+$/', $this->username, $matches);
-        if (!(count($matches) > 0)) {
-            throw new \InvalidSymbolsUsernameException('User entered username with invalid characters');
-        }
 
         if (strlen($this->username) < 3) {
             throw new \UsernameTooShortException('User entered a too short username');
         }
+
+        if (!(count($matches) > 0)) {
+            throw new \InvalidSymbolsUsernameException('User entered username with invalid characters');
+        }
+
     }
 
     private function validatePassword() {
+        
+        if (strlen($this->password) < 6) {
+            throw new \PasswordTooShortException('User entered a password that was too short');
+        }
+
         if ($this->password !== $this->passwordMatch) {
             throw new \PasswordsDoNotMatchException('User entered two different passwords.');
         }
 
-        if (strlen($this->password) < 6) {
-            throw new \PasswordTooShortException('User entered a password that was too short');
-        }
     }
 
     public function stripUsername(string $username) : string {
